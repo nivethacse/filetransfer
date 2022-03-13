55d6a94730e22b00b32f4db050d8eb57c63ca9f3
Nivetha s <akshaya2d@gmail.com>
2022-03-13 19:37:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
 import FileUploadPage from './FileUploadPage';
+import { about } from './components/about';
 
 export const App = () => {
 	return (
 		<div>
 			<Header />
 			<FileUploadPage />
+		        <about/>
 		</div>
 	);
 };
