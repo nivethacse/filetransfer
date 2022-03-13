32a196fedae71e2bc355cf7431ebc9e6c083d410
Nivetha s <akshaya2d@gmail.com>
2022-03-13 20:09:29 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,21 +1,17 @@
 import React from 'react';
 
-
-export const about = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-light  container">
-				<a
-					className="navbar-brand "
-					href="/"
-					style={{ marginLeft: '15px', fontWeight: 'bolder', fontSize: '22px' }}
-				>
-					about
-					
-				</a>
-
-			</nav>
-		</div>
-	);
-};
-
+export default function about() {
+return (
+		<div className="container main-div">
+			<div className="file-icon">
+				<i className="fa fa-5x fa-cloud-upload" aria-hidden="true"></i>
+			</div>
+			<p className="title-text">Share.io</p>
+			<div>
+				<p className="text text-center">
+					{' '}
+					Super simple file sharing Convenient, anonymous and secure
+				</p>
+			</div>
+);
+}
