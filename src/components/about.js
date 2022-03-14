b6e0591d0a9e37bf57c2aefefc016b11a8b0bb1c
Nivetha s <akshaya2d@gmail.com>
2022-03-14 21:00:08 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -3,17 +3,11 @@ import React from 'react';
 export default function about() {
 return (
 	
-	
-		<div className="container">
-			<div className="file-icon">
-				<i className="fa fa-5x fa-cloud-upload" aria-hidden="true"></i>
-			</div>
-			<p className="title-text">about</p>
-			<div>
-				<p className="text text-center">
-					{' '}
-					Super simple file sharing Convenient, anonymous and secure
-				</p>
-			</div>
+	<div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is post on
+        <a href="http://forum.kirupa.com">forums</a>.
+        </p>
+        </div>
 );
 }
