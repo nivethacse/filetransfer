966d8cd21b69428d85446a96470017af6af424eb
Nivetha s <akshaya2d@gmail.com>
2022-03-14 20:21:46 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,17 @@ export const Header = () => {
 
                              
 			</nav>
+                        <nav className="navbar navbar-light  container">
+				<a
+					className="navbar-brand "
+					href="/"
+					style={{ marginLeft: '15px', fontWeight: 'bolder', fontSize: '22px' }}
+				>
+					about
+				</a>
+
+                             
+			</nav>
 		</div>
 	);
 };
