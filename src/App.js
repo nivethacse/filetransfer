29b0a6fc7355ca68f371e3656278b7979de6dfd9
Nivetha s <akshaya2d@gmail.com>
2022-03-14 22:26:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Header } from './components/Header';
 import FileUploadPage from './FileUploadPage';
 import { about } from './components/about';
-
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 
 export const App = () => {
 	return (
 		<div>
+		<Router>
+           <div className="App">
+		<li>
+                <Link to="/about">About Us</Link>
+              </li> 
+           <Routes>
+                
+                 <Route exact path='/about' element={< about />}></Route>
+                
+          </Routes>
+          </div>
+       </Router>
 			<Header />
 			<FileUploadPage />
-		        <about/>
+		        
 		</div>
 	);
 };
