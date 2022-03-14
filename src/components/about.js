727da9befde07eb0d9b20c74820e1577826c0c53
Nivetha s <akshaya2d@gmail.com>
2022-03-14 20:24:00 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -4,19 +4,6 @@ export default function about() {
 return (
 	
 	
-	<div>
-			<nav className="navbar navbar-light  container">
-				<a
-					className="navbar-brand "
-					href="#about"
-					style={{ marginLeft: '15px', fontWeight: 'bolder', fontSize: '22px' }}
-				>
-					about
-				</a>
-
-                             
-			</nav>
-		</div>
 		<div className="container">
 			<div className="file-icon">
 				<i className="fa fa-5x fa-cloud-upload" aria-hidden="true"></i>
