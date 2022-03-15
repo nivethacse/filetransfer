a6453c1ca4e9ec7fef625e0caf07d2b2104def68
KUSHAD CHAKRABORTY <60665405+KUSHAD@users.noreply.github.com>
2022-03-15 13:17:04 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-light  container">
-				<a
-					className="navbar-brand "
-					href="/"
-					style={{ marginLeft: '15px', fontWeight: 'bolder', fontSize: '22px' }}
-				>
-					Share.io
-				</a>
-
-                             
-			</nav>
-                        <nav className="navbar navbar-light  container">
-				<a
-					className="navbar-brand "
-					href="src/components/about.js"
-					style={{ marginright: '15px', fontWeight: 'bolder', fontSize: '22px' }}
-				>
-					about
-				</a>
-
-                             
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="navbar navbar-light  container">
+        <Link
+          className="navbar-brand "
+          to="/"
+          style={{ marginLeft: "15px", fontWeight: "bolder", fontSize: "22px" }}
+        >
+          Share.io
+        </Link>
+      </nav>
+      <nav className="navbar navbar-light  container">
+        <Link
+          to="/about"
+          className="navbar-brand "
+          style={{
+            marginright: "15px",
+            fontWeight: "bolder",
+            fontSize: "22px"
+          }}
+        >
+          about
+        </Link>
+      </nav>
+    </div>
+  );
 };
