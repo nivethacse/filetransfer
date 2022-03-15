cfa406c9deb6e47f87b9ac6ca55ea08c4ecbb0ed
Nivetha s <akshaya2d@gmail.com>
2022-03-15 15:31:05 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h2></h2>
+      <h2>about</h2>
       <p>
         Implement the File Sharing API. This web application will helps to user to upload the
 files.
